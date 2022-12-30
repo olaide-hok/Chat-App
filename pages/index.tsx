a5249a45cd858e3024b365c8a9a6701997e54363
Habeeb kareem <olaidekareem43@yahoo.com>
2022-12-30 17:26:49 +0100

    Resrtuctured IndexPage component.
    Rendered Navigation component.
    
    Rendered ChatHistorySidebar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import {HStack, Flex} from '@chakra-ui/react'
+import ChatHistorySidebar from '../components/ChatHistory/ChatHistorySidebar'
+import Navigation from '../components/Navigation'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <HStack h="100vh" spacing={0}>
+    <Flex as="nav" h="full" maxW={16} w="full" bg="gray.100">
+      <Navigation />
+    </Flex>
+    <Flex
+      as="aside"
+      h="full"
+      maxW="sm"
+      w="full"
+      borderRightColor="gray.100"
+      borderRightWidth={1}
+      pt={8}>
+      <ChatHistorySidebar />
+    </Flex>
+    <Flex
+      as="main"
+      h="full"
+      maxW={16}
+      flex={1}
+      borderRightColor="gray.100"
+      borderRightWidth={1}
+    />
+    <Flex as="aside" h="full" maxW="sm" w="full" />
+  </HStack>
 )
 
 export default IndexPage
