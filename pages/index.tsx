fc6f490e386d57f9ba8aefb2b090a40041283484
Habeeb kareem <olaidekareem43@yahoo.com>
2023-01-01 06:39:45 +0100

    Complete ChatApp project.
    Added Drawer toggle features for Chat & Files sections on md screens.
    
    Added responsiveness to ChatHistorySidebar and ChatFiles sections.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,64 @@
-import {HStack, Flex} from '@chakra-ui/react'
+import {HStack, Flex, useDisclosure} from '@chakra-ui/react'
+import Chat from '../components/Chat'
+import ChatFiles from '../components/ChatFiles'
+import ChatFilesDrawer from '../components/ChatFiles/ChatFilesDrawer'
+import ChatHistoryDrawer from '../components/ChatHistory/ChatHistoryDrawer'
 import ChatHistorySidebar from '../components/ChatHistory/ChatHistorySidebar'
 import Navigation from '../components/Navigation'
 
-const IndexPage = () => (
-  <HStack h="100vh" spacing={0}>
-    <Flex as="nav" h="full" maxW={16} w="full" bg="gray.100">
-      <Navigation />
-    </Flex>
-    <Flex
-      as="aside"
-      h="full"
-      maxW="sm"
-      w="full"
-      borderRightColor="gray.100"
-      borderRightWidth={1}
-      pt={8}>
-      <ChatHistorySidebar />
-    </Flex>
-    <Flex
-      as="main"
-      h="full"
-      maxW={16}
-      flex={1}
-      borderRightColor="gray.100"
-      borderRightWidth={1}
-    />
-    <Flex as="aside" h="full" maxW="sm" w="full" />
-  </HStack>
-)
+const IndexPage = () => {
+  const {
+    isOpen: isChatHistoryOpen,
+    onOpen: onChatHistoryOpen,
+    onClose: onChatHistoryClose,
+  } = useDisclosure()
+  const {
+    isOpen: isChatFilesOpen,
+    onOpen: onChatFilesOpen,
+    onClose: onChatFilesClose,
+  } = useDisclosure()
+  return (
+    <HStack h="100vh" spacing={0}>
+      <Flex as="nav" h="full" maxW={16} w="full" bg="gray.100">
+        <Navigation />
+      </Flex>
+      <Flex
+        as="aside"
+        h="full"
+        maxW={{base: 'xs', xl: 'sm'}}
+        display={{base: 'none', lg: 'flex'}}
+        w="full"
+        borderRightColor="gray.100"
+        borderRightWidth={1}
+        pt={8}>
+        <ChatHistorySidebar />
+      </Flex>
+      <Flex
+        as="main"
+        h="full"
+        flex={1}
+        borderRightColor="gray.100"
+        borderRightWidth={1}>
+        <Chat
+          onChatHistoryOpen={onChatHistoryOpen}
+          onChatFilesOpen={onChatFilesOpen}
+        />
+      </Flex>
+      <Flex
+        as="aside"
+        h="full"
+        maxW={{base: 'xs', xl: 'sm'}}
+        display={{base: 'none', lg: 'flex'}}
+        w="full">
+        <ChatFiles />
+      </Flex>
+      <ChatHistoryDrawer
+        isOpen={isChatHistoryOpen}
+        onClose={onChatHistoryClose}
+      />
+      <ChatFilesDrawer isOpen={isChatFilesOpen} onClose={onChatFilesClose} />
+    </HStack>
+  )
+}
 
 export default IndexPage
